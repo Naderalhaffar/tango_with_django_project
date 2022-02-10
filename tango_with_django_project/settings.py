8bdee73677eb629aa9c0e9239f17eadcf1c896c5
MohammedNader AlHaffar <2581031A@student.gla.ac.ukgit clone https://github.com/Naderalhaffar/tango_with_django_project.gitexitexit()git config --global user.name MohammedNader>
2022-02-10 18:40:43 +0000

    Chapter 10 Tests

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -122,6 +122,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
+SESSION_COOKIE_AGE = 1209600
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
