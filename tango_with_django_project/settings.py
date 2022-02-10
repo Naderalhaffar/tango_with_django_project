5eaebae482d7ecfb329f88bd2d721f85b43328b4
MohammedNader AlHaffar <2581031A@student.gla.ac.ukgit clone https://github.com/Naderalhaffar/tango_with_django_project.gitexitexit()git config --global user.name MohammedNader>
2022-02-10 00:00:08 +0000

    Chapter 9 Tests

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -84,6 +84,10 @@ DATABASES = {
     }
 }
 
+PASSWORD_HASHERS = (
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -94,6 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {'min_length': 6,}
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
@@ -120,6 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+LOGIN_URL = 'rango:login'
 STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
 MEDIA_ROOT = MEDIA_DIR
